c09c7ec795a8ff3d95eacf523308ddc7748a116e
alissonmediavilla214 <armediavilla@puce.edu.ec>
2026-02-24 15:39:03 -0500

    Final Prewiev

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ const clientes = [
     { nombre: "Pablo Pérez", edad: 15, tickets: 2 },     // Menor
     { nombre: "María López", edad: 70, tickets: 1 },     // Mayor
     // TODO: Agrega 4 clientes más siguiendo esta estructura
+    { nombre: "Ulises Yepes", edad: 40, tickets: 5},
+    { nombre: "Manzero Montoya", edad: 80, tickets: 10},
+    { nombre: "Jeremias Monarca", edad: 29, tickets: 6},
+    { nombre: "Momo Valenzuela", edad: 15, tickets: 13},
+
     // Asegúrate de incluir: 1 menor más, 1 mayor más, y 2 adultos (18-64 años)
 ];
 
@@ -43,17 +48,23 @@ for (let i = 0; i < clientes.length; i++) {
     
     // TODO: Paso 2 - Calcula el precio sin descuento
     // Multiplica PRECIO_BASE por cantidadTickets
-    let precioSinDescuento = 0; // Reemplaza 0 con el cálculo correcto
+    let precioSinDescuento = PRECIO_BASE*cantidadTickets; // Reemplaza 0 con el cálculo correcto
     
     // TODO: Paso 3 - Calcula el descuento según la edad
     let descuento = 0;
     // Si edad < 18, descuento = 0.50 (50%)
+    if (edad < 18) {
+        descuento = 0.50;
+    } 
     // Si edad >= 65, descuento = 0.30 (30%)
+    else if (edad >= 65) {
+        descuento = 0.30
+    }
     // Si no, descuento = 0 (sin descuento)
     
     // TODO: Paso 4 - Calcula el precio con descuento
     // Resta al precioSinDescuento el monto del descuento
-    let precioConDescuento = 0; // Reemplaza 0 con el cálculo correcto
+    let precioConDescuento = precioSinDescuento - (precioSinDescuento * descuento); // Reemplaza 0 con el cálculo correcto
     
     // TODO: Paso 5 - Muestra el reporte del cliente
     console.log("Cliente: " + nombre);
